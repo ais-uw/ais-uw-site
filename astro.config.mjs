45fd96deccdb3d481030230e8a344160870c43bb
Robby Ingebretsen <robby@thinkpixellab.com>
2025-04-20 19:19:45 -0700

    Build action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import staticAdapter from "@astrojs/adapter-static";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        defaultLayout: "./src/layouts/Default.astro",
-    },
-
     vite: {
         plugins: [tailwindcss()],
     },
+
+    adapter: staticAdapter(),
+    base: process.env.GITHUB_REPOSITORY
+        ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
+        : "/",
 });
